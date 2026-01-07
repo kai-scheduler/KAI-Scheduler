2e477710f85276c71e44c09c048b074c8db2fed2
AdheipSingh <adheip1222@gmail.com>
2026-01-08 03:32:57 +0530

    add queue validator
    Signed-off-by: AdheipSingh <adheip1222@gmail.com>

---
+++ pkg/apis/scheduling/v2/queue_validator.go
@@ -0,0 +1,219 @@
+// Copyright 2025 NVIDIA CORPORATION
+// SPDX-License-Identifier: Apache-2.0
+
+package v2
+
+import (
+	"context"
+	"fmt"
+
+	"k8s.io/apimachinery/pkg/runtime"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+	logf "sigs.k8s.io/controller-runtime/pkg/log"
+	"sigs.k8s.io/controller-runtime/pkg/webhook/admission"
+)
+
+var queueValidatorLog = logf.Log.WithName("queue-validator")
+
+type QueueValidator interface {
+	ValidateCreate(ctx context.Context, obj runtime.Object) (warnings admission.Warnings, err error)
+	ValidateUpdate(ctx context.Context, oldObj, newObj runtime.Object) (warnings admission.Warnings, err error)
+	ValidateDelete(ctx context.Context, obj runtime.Object) (warnings admission.Warnings, err error)
+}
+
+type queueValidator struct {
+	kubeClient client.Client
+}
+
+func NewQueueValidator(kubeClient client.Client) QueueValidator {
+	return &queueValidator{
+		kubeClient: kubeClient,
+	}
+}
+
+func (v *queueValidator) ValidateCreate(ctx context.Context, obj runtime.Object) (admission.Warnings, error) {
+	queue, ok := obj.(*Queue)
+	if !ok {
+		return nil, fmt.Errorf("expected a Queue but got a %T", obj)
+	}
+	queueValidatorLog.Info("validate create", "name", queue.Name)
+
+	if queue.Spec.Resources == nil {
+		return []string{missingResourcesError}, fmt.Errorf(missingResourcesError)
+	}
+
+	// Validate parent-child CPU quota relationship
+	if queue.Spec.ParentQueue != "" {
+		return v.validateParentChildQuota(ctx, queue)
+	}
+
+	return nil, nil
+}
+
+func (v *queueValidator) ValidateUpdate(ctx context.Context, oldObj, newObj runtime.Object) (admission.Warnings, error) {
+	oldQueue, ok := oldObj.(*Queue)
+	if !ok {
+		return nil, fmt.Errorf("expected a Queue but got a %T", oldObj)
+	}
+	
+	newQueue, ok := newObj.(*Queue)
+	if !ok {
+		return nil, fmt.Errorf("expected a Queue but got a %T", newObj)
+	}
+	queueValidatorLog.Info("validate update", "name", newQueue.Name)
+
+	if newQueue.Spec.Resources == nil {
+		return []string{missingResourcesError}, fmt.Errorf(missingResourcesError)
+	}
+
+	var warnings admission.Warnings
+
+	// Validate parent-child CPU quota relationship if parent exists
+	if newQueue.Spec.ParentQueue != "" {
+		parentWarnings, err := v.validateParentChildQuota(ctx, newQueue)
+		if err != nil {
+			return parentWarnings, err
+		}
+		warnings = append(warnings, parentWarnings...)
+	}
+
+	// If this queue has children, validate that new quota >= sum of children quotas
+	if len(oldQueue.Status.ChildQueues) > 0 {
+		childWarnings, err := v.validateChildrenQuotaSum(ctx, newQueue)
+		if err != nil {
+			return childWarnings, err
+		}
+		warnings = append(warnings, childWarnings...)
+	}
+
+	return warnings, nil
+}
+
+func (v *queueValidator) ValidateDelete(ctx context.Context, obj runtime.Object) (admission.Warnings, error) {
+	queue, ok := obj.(*Queue)
+	if !ok {
+		return nil, fmt.Errorf("expected a Queue but got a %T", obj)
+	}
+	queueValidatorLog.Info("validate delete", "name", queue.Name)
+	
+	// Prevent deletion if queue has children
+	if len(queue.Status.ChildQueues) > 0 {
+		return nil, fmt.Errorf("cannot delete queue %s: it has child queues %v", queue.Name, queue.Status.ChildQueues)
+	}
+	
+	return nil, nil
+}
+
+func (v *queueValidator) validateParentChildQuota(ctx context.Context, childQueue *Queue) (admission.Warnings, error) {
+	// Fetch parent queue
+	parentQueue := &Queue{}
+	err := v.kubeClient.Get(ctx, client.ObjectKey{Name: childQueue.Spec.ParentQueue}, parentQueue)
+	if err != nil {
+		return nil, fmt.Errorf("failed to get parent queue %s: %w", childQueue.Spec.ParentQueue, err)
+	}
+
+	// Validate parent has resources defined
+	if parentQueue.Spec.Resources == nil {
+		return nil, fmt.Errorf("parent queue %s has no resources defined", parentQueue.Name)
+	}
+
+	warnings := []string{}
+
+	// Validate child CPU quota <= parent CPU quota
+	childCPU := childQueue.Spec.Resources.CPU.Quota
+	parentCPU := parentQueue.Spec.Resources.CPU.Quota
+	
+	if childCPU > parentCPU {
+		return nil, fmt.Errorf("child queue CPU quota (%.0f) exceeds parent queue %s CPU quota (%.0f)", 
+			childCPU, parentQueue.Name, parentCPU)
+	}
+
+	// Calculate sum of all children's CPU quotas
+	totalChildrenCPU := childCPU
+	for _, childName := range parentQueue.Status.ChildQueues {
+		// Skip if this is the current queue being validated (for updates)
+		if childName == childQueue.Name {
+			continue
+		}
+		
+		existingChild := &Queue{}
+		if err := v.kubeClient.Get(ctx, client.ObjectKey{Name: childName}, existingChild); err != nil {
+			queueValidatorLog.Error(err, "failed to get child queue", "child", childName)
+			continue
+		}
+		
+		if existingChild.Spec.Resources != nil {
+			totalChildrenCPU += existingChild.Spec.Resources.CPU.Quota
+		}
+	}
+
+	// Validate total children CPU quotas <= parent CPU quota
+	if totalChildrenCPU > parentCPU {
+		warnings = append(warnings, fmt.Sprintf("total children CPU quota (%.0f) exceeds parent queue %s CPU quota (%.0f) - this may cause pod scheduling failures", 
+			totalChildrenCPU, parentQueue.Name, parentCPU))
+	}
+
+	// Also validate GPU and Memory quotas
+	if childQueue.Spec.Resources.GPU.Quota > parentQueue.Spec.Resources.GPU.Quota {
+		return nil, fmt.Errorf("child queue GPU quota (%.2f) exceeds parent queue %s GPU quota (%.2f)", 
+			childQueue.Spec.Resources.GPU.Quota, parentQueue.Name, parentQueue.Spec.Resources.GPU.Quota)
+	}
+
+	if childQueue.Spec.Resources.Memory.Quota > parentQueue.Spec.Resources.Memory.Quota {
+		return nil, fmt.Errorf("child queue Memory quota (%.0f) exceeds parent queue %s Memory quota (%.0f)", 
+			childQueue.Spec.Resources.Memory.Quota, parentQueue.Name, parentQueue.Spec.Resources.Memory.Quota)
+	}
+
+	return warnings, nil
+}
+
+func (v *queueValidator) validateChildrenQuotaSum(ctx context.Context, parentQueue *Queue) (admission.Warnings, error) {
+	warnings := []string{}
+	
+	if parentQueue.Spec.Resources == nil {
+		return nil, fmt.Errorf("parent queue %s has no resources defined", parentQueue.Name)
+	}
+
+	totalChildrenCPU := float64(0)
+	totalChildrenGPU := float64(0)
+	totalChildrenMemory := float64(0)
+
+	// Calculate sum of all children quotas
+	for _, childName := range parentQueue.Status.ChildQueues {
+		child := &Queue{}
+		if err := v.kubeClient.Get(ctx, client.ObjectKey{Name: childName}, child); err != nil {
+			queueValidatorLog.Error(err, "failed to get child queue", "child", childName)
+			continue
+		}
+		
+		if child.Spec.Resources != nil {
+			totalChildrenCPU += child.Spec.Resources.CPU.Quota
+			totalChildrenGPU += child.Spec.Resources.GPU.Quota
+			totalChildrenMemory += child.Spec.Resources.Memory.Quota
+			
+			// Check if individual child exceeds parent
+			if child.Spec.Resources.CPU.Quota > parentQueue.Spec.Resources.CPU.Quota {
+				warnings = append(warnings, fmt.Sprintf("child queue %s CPU quota (%.0f) exceeds parent CPU quota (%.0f)", 
+					childName, child.Spec.Resources.CPU.Quota, parentQueue.Spec.Resources.CPU.Quota))
+			}
+		}
+	}
+
+	// Validate total children quotas don't exceed parent
+	if totalChildrenCPU > parentQueue.Spec.Resources.CPU.Quota {
+		warnings = append(warnings, fmt.Sprintf("total children CPU quota (%.0f) exceeds parent CPU quota (%.0f) - this may cause pod scheduling failures", 
+			totalChildrenCPU, parentQueue.Spec.Resources.CPU.Quota))
+	}
+
+	if totalChildrenGPU > parentQueue.Spec.Resources.GPU.Quota {
+		warnings = append(warnings, fmt.Sprintf("total children GPU quota (%.2f) exceeds parent GPU quota (%.2f) - this may cause pod scheduling failures", 
+			totalChildrenGPU, parentQueue.Spec.Resources.GPU.Quota))
+	}
+
+	if totalChildrenMemory > parentQueue.Spec.Resources.Memory.Quota {
+		warnings = append(warnings, fmt.Sprintf("total children Memory quota (%.0f) exceeds parent Memory quota (%.0f) - this may cause pod scheduling failures", 
+			totalChildrenMemory, parentQueue.Spec.Resources.Memory.Quota))
+	}
+
+	return warnings, nil
+}
--- pkg/apis/scheduling/v2/queue_webhook.go
+++ pkg/apis/scheduling/v2/queue_webhook.go
@@ -4,13 +4,8 @@
 package v2
 
 import (
-	"context"
-	"fmt"
-
-	"k8s.io/apimachinery/pkg/runtime"
 	ctrl "sigs.k8s.io/controller-runtime"
 	logf "sigs.k8s.io/controller-runtime/pkg/log"
-	"sigs.k8s.io/controller-runtime/pkg/webhook/admission"
 )
 
 // log is for logging in this package.
@@ -21,42 +16,45 @@ const missingResourcesError = "resources must be specified"
 func (r *Queue) SetupWebhookWithManager(mgr ctrl.Manager) error {
 	return ctrl.NewWebhookManagedBy(mgr).
 		For(r).
-		WithValidator(&Queue{}).
+		WithValidator(NewQueueValidator(mgr.GetClient())).
 		Complete()
 }
 
-func (_ *Queue) ValidateCreate(_ context.Context, obj runtime.Object) (admission.Warnings, error) {
-	queue, ok := obj.(*Queue)
-	if !ok {
-		return nil, fmt.Errorf("expected a Queue but got a %T", obj)
-	}
-	queuelog.Info("validate create", "name", queue.Name)
+// TODO: Remove after QueueValidator is fully integrated
+// func (_ *Queue) ValidateCreate(_ context.Context, obj runtime.Object) (admission.Warnings, error) {
+// 	queue, ok := obj.(*Queue)
+// 	if !ok {
+// 		return nil, fmt.Errorf("expected a Queue but got a %T", obj)
+// 	}
+// 	queuelog.Info("validate create", "name", queue.Name)
 
-	if queue.Spec.Resources == nil {
-		return []string{missingResourcesError}, fmt.Errorf(missingResourcesError)
-	}
-	return nil, nil
-}
+// 	if queue.Spec.Resources == nil {
+// 		return []string{missingResourcesError}, fmt.Errorf(missingResourcesError)
+// 	}
+// 	return nil, nil
+// }
 
 // ValidateUpdate implements webhook.Validator so a webhook will be registered for the type
-func (_ *Queue) ValidateUpdate(_ context.Context, _ runtime.Object, newObj runtime.Object) (admission.Warnings, error) {
-	queue, ok := newObj.(*Queue)
-	if !ok {
-		return nil, fmt.Errorf("expected a Queue but got a %T", newObj)
-	}
-	queuelog.Info("validate update", "name", queue.Name)
+// TODO: Remove after QueueValidator is fully integrated
+// func (_ *Queue) ValidateUpdate(_ context.Context, _ runtime.Object, newObj runtime.Object) (admission.Warnings, error) {
+// 	queue, ok := newObj.(*Queue)
+// 	if !ok {
+// 		return nil, fmt.Errorf("expected a Queue but got a %T", newObj)
+// 	}
+// 	queuelog.Info("validate update", "name", queue.Name)
 
-	if queue.Spec.Resources == nil {
-		return []string{missingResourcesError}, fmt.Errorf(missingResourcesError)
-	}
-	return nil, nil
-}
+// 	if queue.Spec.Resources == nil {
+// 		return []string{missingResourcesError}, fmt.Errorf(missingResourcesError)
+// 	}
+// 	return nil, nil
+// }
 
-func (_ *Queue) ValidateDelete(_ context.Context, obj runtime.Object) (admission.Warnings, error) {
-	queue, ok := obj.(*Queue)
-	if !ok {
-		return nil, fmt.Errorf("expected a Queue but got a %T", obj)
-	}
-	queuelog.Info("validate delete", "name", queue.Name)
-	return nil, nil
-}
+// TODO: Remove after QueueValidator is fully integrated
+// func (_ *Queue) ValidateDelete(_ context.Context, obj runtime.Object) (admission.Warnings, error) {
+// 	queue, ok := obj.(*Queue)
+// 	if !ok {
+// 		return nil, fmt.Errorf("expected a Queue but got a %T", obj)
+// 	}
+// 	queuelog.Info("validate delete", "name", queue.Name)
+// 	return nil, nil
+// }
