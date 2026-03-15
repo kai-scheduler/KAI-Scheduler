512faa98a3ff54f38bda743914887eb0f4ea2515
SiorMeir <msior@nvidia.com>
2026-03-15 20:35:43 +0200

    test: adjust modules for scale tests, fix queue names
    Signed-off-by: SiorMeir <msior@nvidia.com>

--- test/e2e/modules/utils/name_generators.go
+++ test/e2e/modules/utils/name_generators.go
@@ -6,22 +6,32 @@ package utils
 
 import (
 	"math/rand"
+	"sync"
 	"time"
 )
 
-var generatedNames = make(map[string]bool)
+var (
+	generatedNames     = make(map[string]bool)
+	generatedNamesLock sync.Mutex
+)
 
 func GenerateRandomK8sName(l int) string {
 	str := "abcdefghijklmnopqrstuvwxyz"
-	bytes := []byte(str)
-	var result []byte
+	chars := []byte(str)
 	r := rand.New(rand.NewSource(time.Now().UnixNano()))
-	for i := 0; i < l; i++ {
-		result = append(result, bytes[r.Intn(len(bytes))])
+	for {
+		result := make([]byte, l)
+		for i := range l {
+			result[i] = chars[r.Intn(len(chars))]
+		}
+		name := string(result)
+
+		generatedNamesLock.Lock()
+		if !generatedNames[name] {
+			generatedNames[name] = true
+			generatedNamesLock.Unlock()
+			return name
+		}
+		generatedNamesLock.Unlock()
 	}
-	if generatedNames[string(result)] {
-		return GenerateRandomK8sName(l)
-	}
-	generatedNames[string(result)] = true
-	return string(result)
 }
