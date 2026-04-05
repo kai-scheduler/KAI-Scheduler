02078e206af5a5ee2f026f7fe5dc0c8969b9706f
davidLif <davidshani12@gmail.com>
2026-04-05 21:57:55 +0300

    At the podgroup level, if not nullable, minMember and MinSubGroup are minimum 1
    Signed-off-by: davidLif <davidshani12@gmail.com>

--- pkg/apis/scheduling/v2alpha2/podgroup_types.go
+++ pkg/apis/scheduling/v2alpha2/podgroup_types.go
@@ -36,14 +36,14 @@ type PodGroupSpec struct {
 	// if there are not enough resources to start all required members, the scheduler will not start anyone.
 	// Mutually exclusive with MinSubGroup.
 	// +kubebuilder:validation:Nullable
-	// +kubebuilder:validation:Minimum=0
+	// +kubebuilder:validation:Minimum=1
 	MinMember *int32 `json:"minMember,omitempty" protobuf:"varint,1,opt,name=minMember"`
 
 	// MinSubGroup defines the minimal number of direct child SubGroups required for this PodGroup to be schedulable.
 	// Only applicable when SubGroups are defined.
 	// Mutually exclusive with MinMember.
 	// +kubebuilder:validation:Optional
-	// +kubebuilder:validation:Minimum=0
+	// +kubebuilder:validation:Minimum=1
 	MinSubGroup *int32 `json:"minSubGroup,omitempty"`
 
 	// Queue defines the queue to allocate resource for PodGroup; if queue does not exist,
