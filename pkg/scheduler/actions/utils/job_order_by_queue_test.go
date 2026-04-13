1d6cf5b34ba38781e4d98d4fd74ead92cfd85fc0
itsomri <omric@nvidia.com>
2026-04-13 14:48:11 +0300

    Fix cache calculation for test
    Signed-off-by: itsomri <omric@nvidia.com>

--- pkg/scheduler/actions/utils/job_order_by_queue_test.go
+++ pkg/scheduler/actions/utils/job_order_by_queue_test.go
@@ -755,6 +755,7 @@ func TestJobsOrderByQueues_RequeueJob(t *testing.T) {
 			jobsOrder.PushJob(jobToRequeue)
 
 			for _, expectedJob := range tt.expected.expectedJobsList {
+				_ = expectedJob.GetAllPodsMap()
 				actualJob := jobsOrder.PopNextJob()
 				assert.Equal(t, expectedJob, actualJob)
 			}
