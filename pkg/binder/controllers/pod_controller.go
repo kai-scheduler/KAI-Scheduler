d388eff3faea4001e8b3bb226f18906faddf8ddf
Erez Freiberger <enoodle@gmail.com>
2026-08-08 00:35:01 +0200

    fix(binder): preserve pod RBAC generation
    Signed-off-by: Erez Freiberger <enoodle@gmail.com>

--- pkg/binder/controllers/pod_controller.go
+++ pkg/binder/controllers/pod_controller.go
@@ -39,6 +39,7 @@ type ReconcilerParams struct {
 // +kubebuilder:rbac:groups=core,resources=pods,verbs=get;list;watch;create;update;patch;delete
 // +kubebuilder:rbac:groups=core,resources=pods/status,verbs=get;update;patch
 // +kubebuilder:rbac:groups=core,resources=pods/finalizers,verbs=update
+
 // Reconcile is part of the main kubernetes reconciliation loop which aims to
 // move the current state of the cluster closer to the desired state.
 // For more details, check Reconcile and its Result here:
