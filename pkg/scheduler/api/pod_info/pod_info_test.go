7bcc2d2a989f70e0c1a53999bfa3bee2df06d532
davidLif <davidshani12@gmail.com>
2026-08-10 11:24:47 +0300

    feat(scheduler): enforce fractional GPU compute modes
    Signed-off-by: davidLif <davidshani12@gmail.com>

--- pkg/scheduler/api/pod_info/pod_info_test.go
+++ pkg/scheduler/api/pod_info/pod_info_test.go
@@ -527,7 +527,6 @@ func TestPodInfo_updatePodAdditionalFields(t *testing.T) {
 				Status:         tt.fields.Status,
 				Pod:            tt.fields.Pod,
 				GpuRequirement: *resource_info.NewGpuResourceRequirement(),
-				GPUGroups:      make([]string, 0),
 				VectorMap:      vectorMap,
 			}
 			pi.updatePodAdditionalFields(tt.fields.bindingRequest)
@@ -541,9 +540,9 @@ func TestPodInfo_updatePodAdditionalFields(t *testing.T) {
 					tt.name, tt.expected.AcceptedGpuRequirement, pi.AcceptedGpuRequirement)
 			}
 			assert.Equal(t, string(pi.ResourceRequestType), tt.expected.ResourceRequestType)
-			if !reflect.DeepEqual(pi.GPUGroups, tt.expected.GPUGroups) {
+			if !reflect.DeepEqual(pi.GPUGroupIDs(), tt.expected.GPUGroups) {
 				t.Errorf("case (%s) failed: GPUGroups \n expected %v, \n got: %v \n",
-					tt.name, tt.expected.GPUGroups, pi.GPUGroups)
+					tt.name, tt.expected.GPUGroups, pi.GPUGroupIDs())
 			}
 		})
 	}
